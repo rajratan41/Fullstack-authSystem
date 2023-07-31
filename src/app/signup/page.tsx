c4696073e271af00815f55def94f5ba4314ec76e
Raj Ratan <raj808341@gmail.com>
2023-07-31 15:29:18 +0530

    Signup Page Updated

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,7 +1,56 @@
+"use client";
+import Link from "next/link";
+import React from "react";
+import { useRouter } from "next/navigation";
+import { axios } from "axios";
+
 export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: "",
+    password: "",
+    username: "",
+  });
+
+  const onSignup = async () => {};
+
   return (
-    <div>
-      <h1 className="text-center text-2xl">Signup</h1>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="mb-4">Signup</h1>
+      <hr />
+      <label htmlFor="username">username</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        type="text"
+        id="username"
+        placeholder="username"
+        value={user.username}
+        onChange={(e) => setUser({ ...user, username: e.target.value })}
+      />
+      <label htmlFor="email">email</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        type="text"
+        id="email"
+        placeholder="email"
+        value={user.email}
+        onChange={(e) => setUser({ ...user, email: e.target.value })}
+      />
+      <label htmlFor="password">password</label>
+      <input
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600 text-black"
+        type="password"
+        id="password"
+        placeholder="password"
+        value={user.password}
+        onChange={(e) => setUser({ ...user, password: e.target.value })}
+      />
+      <button
+        onClick={onSignup}
+        className="p-2 border border-gray-300 rounded-lg mb-4 focus:outline-none focus:border-gray-600"
+      >
+        Signup Here
+      </button>
+      <Link href="/login">Visit Login Page</Link>
     </div>
   );
 }
