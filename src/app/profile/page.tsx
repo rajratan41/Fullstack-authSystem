68b384b327b2881f990eafe15dc9646075c81298
Raj Ratan <raj808341@gmail.com>
2023-08-03 20:52:01 +0530

    added getUser function

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import axios from "axios";
 import Link from "next/link";
+import React, { useState } from "react";
 import { toast } from "react-hot-toast";
 import { useRouter } from "next/navigation";
 
 export default function ProfilePage() {
   const router = useRouter();
+  const [data, setData] = useState("Nothing");
   const logout = async () => {
     try {
       await axios.get("/api/users/logout");
@@ -17,17 +19,36 @@ export default function ProfilePage() {
       toast.error(error.message);
     }
   };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
       <p>Profile Page</p>
+      <h2 className="p-2 mt-2 rounded bg-green-500">
+        {data === "Nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
       <hr />
       <button
         onClick={logout}
         className="bg-orange-500 mt-4 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
       >
         Logout
+      </button>
+      <button
+        onClick={getUserDetails}
+        className="bg-green-600 mt-4 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+      >
+        GetUser Details
       </button>
     </div>
   );
